300ce1fba7f4fffe71f5ef4abf1ffaef01d2fbba
Andrew <khantzay.ly@gmail.com>
2023-07-04 23:20:39 +0630

    Conceptians Bot 0.1

---
+++ README.md
@@ -0,0 +1,69 @@
+# **Conceptions Telegram Bot**
+
+The Conceptions Telegram Bot is a bot designed to provide users with access to a library of books and social media links. Users can choose categories of books they want to read and then select specific books for downloading. Additionally, the bot provides links to Conceptions' social media profiles.
+
+
+## **Features**
+
+
+
+1. **Start Command**: The `/start` command initiates a conversation with the bot and displays a one-time keyboard with options to navigate to the library or social media links.
+2. **Library Command**: The `Library` command displays a list of available book categories using data retrieved from the Conceptions API. Users can select a category to view the books within that category.
+3. **Book Selection**: After selecting a category, users can choose a specific book to download. The bot retrieves book information from the API and displays the book's title, category, file size, and an option to download the book.
+4. **Download Limit**: The bot tracks the number of books a user has downloaded and limits the number of downloads to five. If a user reaches the download limit, they receive a notification indicating that they have reached the maximum allowed downloads.
+5. **Social Media Links**: The `Social Media Links` command provides users with quick access to Conceptions' social media profiles, including Facebook, YouTube, Instagram, LinkedIn, and TikTok.
+
+
+## **Setup**
+
+To run the Conceptions Telegram Bot, you need to perform the following steps:
+
+
+
+1. Set up a Redis server and obtain the host, port, and password.
+2. Create an account on the[ Telegram Bot Platform](https://core.telegram.org/bots) and obtain an API token.
+3. Obtain an API token for the Conceptions API by contacting the administrators of the Conceptions platform.
+4. Set up a server to host the bot code. You can use platforms like[ Heroku](https://www.heroku.com/) or[ Google Cloud Platform](https://cloud.google.com/) for deployment.
+5. Set the environment variables required for the bot:
+    * `TELEGRAM_API`: Telegram API token obtained from the Bot Platform.
+    * `ROUTE_API`: Conceptions API token.
+    * `REDIS_HOST`: Redis server host.
+    * `REDIS_PORT`: Redis server port.
+    * `REDIS_PASSWORD`: Redis server password.
+6. Install the necessary Python packages by running `pip install -r requirements.txt`.
+7. Start the bot by running `python &lt;filename.py>`.
+
+
+## **Usage**
+
+Once the bot is up and running, users can interact with it through Telegram. Here's a step-by-step guide on how to use the bot:
+
+
+
+1. Start a conversation with the bot by searching for its username or by clicking on a provided link.
+2. Upon starting, the bot will greet the user and display a one-time keyboard with options to navigate to the library or social media links.
+3. To access the library, select the `Library` option from the keyboard. The bot will retrieve a list of available book categories.
+4. Choose a category of interest by selecting it from the displayed options. The bot will retrieve books belonging to the selected category.
+5. Select a specific book from the list to view details about the book, such as the title, category, file size, and an option to download the book.
+6. If the user attempts to download more than five books within the same category, they will receive a notification indicating that they have reached the maximum allowed downloads.
+7. To access Conceptions' social media links, select the `Social Media Links` option from the one-time keyboard. The bot will display buttons for various social media platforms.
+8. Click on any of the provided buttons to open the respective social media link.
+
+
+## **Contributing**
+
+Contributions to the Conceptions Telegram Bot are welcome! If you encounter any issues or have suggestions for improvements, please feel free to open an issue or submit a pull request on the project's GitHub repository.
+
+
+## **License**
+
+The Conceptions Telegram Bot is released under the[ MIT License](https://opensource.org/licenses/MIT). You can find more details in the[ LICENSE](https://github.com/example/conceptions-telegram-bot/blob/main/LICENSE) file.
+
+
+## **About**
+
+The Conceptions Telegram Bot was developed by [Your Name] as a project to provide users with easy access to a library of books and Conceptions' social media profiles. It is not an official product of Conceptions but utilizes the Conceptions API for book data.
+
+For more information, contact [Your Name] at [your-email@example.com].
+
+Thank you for using the Conceptions Telegram Bot! We hope you enjoy your reading experience and find the social media links helpful.
---
+++ app.py
@@ -0,0 +1,173 @@
+import json
+import requests
+import redis
+from telebot import types, TeleBot
+from keep_alive import keep_alive
+import os
+
+keep_alive()
+
+
+bot = TeleBot(os.environ["TELEGRAM_API"])
+
+# Connect to Redis
+redis_client = redis.Redis(host=os.environ['REDIS_HOST'],
+                           port=os.environ['REDIS_PORT'],
+                           password=os.environ['REDIS_PASSWORD'])
+
+chat_id = {}
+log_key = 0
+
+
+def add(id, category):
+  # Load the existing data from Redis
+  users_data = redis_client.get("users_data")
+  if users_data:
+    users_data = json.loads(users_data)
+  else:
+    users_data = []
+
+  # Check if the user already exists in the data
+  existing_user = next((player for player in users_data if player["id"] == id),
+                       None)
+
+  if existing_user is not None:
+    # Replace the existing user's category
+    existing_user["category"] = category
+  else:
+    # Create a dictionary for the new player with chance_count set to 0
+    new_user = {"id": id, "category": category, "chance_count": 0}
+    # Append the new user data to the existing data
+    users_data.append(new_user)
+
+  # Save the updated user data to Redis
+  redis_client.setex("users_data", 86400, json.dumps(users_data))
+
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+  # Create a one-time keyboard
+  keyboard = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+  library = types.KeyboardButton('Library')
+  social = types.KeyboardButton('Social Media Links')
+  keyboard.add(library, social)
+
+  # Send the message with the one-time keyboard
+  global log_key
+  log_key = 3
+  bot.send_message(message.chat.id,
+                   'Welcome From Conceptians!',
+                   reply_markup=keyboard)
+
+
+@bot.message_handler(func=lambda message: message.text == 'Library')
+def Library(message):
+  url = f"https://admin.conceptians.org/api/bot/category"
+  headers = {"Authorization": os.environ["ROUTE_API"]}
+  response = requests.get(url, headers=headers)
+  data = response.json()
+  #Create a keyboard with buttons for each category
+  keyboard = types.ReplyKeyboardMarkup(row_width=2, one_time_keyboard=True)
+  for category in data:
+    button = types.KeyboardButton(category['category'])
+    keyboard.add(button)
+  global log_key
+  log_key = 1
+  bot.send_message(message.chat.id,
+                   'Choose Categories that you want to read',
+                   reply_markup=keyboard)
+
+
+@bot.message_handler(func=lambda message: True if log_key == 1 else False)
+def books(message):
+  add(message.chat.id, message.text)
+  keyboard = types.ReplyKeyboardMarkup(row_width=2, one_time_keyboard=True)
+  button = types.KeyboardButton("Library")
+  keyboard.add(button)
+  url = f"https://admin.conceptians.org/api/bot/category/{message.text}"
+  headers = {"Authorization": os.environ["ROUTE_API"]}
+  response = requests.get(url, headers=headers)
+  json_data = response.json()
+  for data in json_data:
+    button = types.KeyboardButton(data['title'])
+    keyboard.add(button)
+  global log_key
+  log_key = 2
+  bot.send_message(message.chat.id,
+                   'Choose Books that you want to download',
+                   reply_markup=keyboard)
+
+
+@bot.message_handler(func=lambda message: True if log_key == 2 else False)
+def download_books(message):
+  # Load the existing data from Redis
+  users_data = redis_client.get("users_data")
+  if users_data:
+    users_data = json.loads(users_data)
+  else:
+    users_data = []
+
+  for user in users_data:
+    if user["id"] == message.chat.id:
+      # Check if the user has reached the maximum chance count
+      if user["chance_count"] > 5:
+        bot.send_message(
+          message.chat.id,
+          "You have reached the maximum number of allowed downloads.")
+        return
+
+      url = f"https://admin.conceptians.org/api/bot/category/{user['category']}"
+      headers = {"Authorization": os.environ["ROUTE_API"]}
+      response = requests.get(url, headers=headers)
+      json_data = response.json()
+      for book in json_data:
+        if book['title'] == message.text:
+          title = f"<b>{book['title']}</b>"
+          cat = f"<i>Category: {book['category']}</i>"
+          filesize = f"<i>File size: {book['filesize']}mb</i>"
+          download = book['link']
+          button = types.InlineKeyboardButton(text='Download', url=download)
+          # Create a keyboard with the button
+          keyboard = types.InlineKeyboardMarkup([[button]])
+          try:
+            photo_url = book['image']
+            bot.send_photo(chat_id=message.chat.id, photo=photo_url)
+          except:
+            print('no photo')
+          bot.send_message(message.chat.id,
+                           f"{title}\n{cat}\n{filesize}",
+                           parse_mode='HTML',
+                           reply_markup=keyboard)
+
+          # Update the chance count for the user
+          user["chance_count"] += 1
+
+  # Save the updated user data back to Redis
+  redis_client.set("users_data", json.dumps(users_data))
+
+
+@bot.message_handler(func=lambda message: message.text == 'Social Media Links')
+def Social_Media_Links(message):
+  keyboard = types.InlineKeyboardMarkup()
+  button1 = types.InlineKeyboardButton(
+    text='Facebook',
+    url='https://www.facebook.com/profile.php?id=100082812927163')
+  button2 = types.InlineKeyboardButton(
+    text='Youtube', url='https://www.youtube.com/@conceptians8961/featured')
+  button3 = types.InlineKeyboardButton(
+    text='Instagram', url='https://www.instagram.com/conceptians_org')
+  button4 = types.InlineKeyboardButton(
+    text='Linkedin', url='https://www.linkedin.com/company/conceptians/')
+  button5 = types.InlineKeyboardButton(
+    text='Tiktok', url='https://www.tiktok.com/@conceptians')
+  keyboard.add(button1, button2)
+  keyboard.add(button3, button4)
+  keyboard.add(button5)
+
+  # Send a message with the inline keyboard
+  bot.send_message(message.chat.id,
+                   "Click the button to open the link:",
+                   reply_markup=keyboard)
+
+
+bot.polling()
